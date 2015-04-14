ebf76d1c4315129b313738d68941184bc251e4c5
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-04-14 12:50:02 +0300

    Improves the linux installer to overwrite files when copying

--- assets/install.sh
+++ assets/install.sh
@@ -5,12 +5,11 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 mkdir -p /usr/share/MessengerForDesktop
-cp ./* /usr/share/MessengerForDesktop/
+yes | cp -rf ./* /usr/share/MessengerForDesktop/
 
 read -p "Add a shortcut to Messenger on your Desktop? [yN]" yn
 
 case $yn in
   [Yy]* ) ln -s /usr/share/MessengerForDesktop/Messenger $HOME/Desktop/Messenger;;
   [Nn]* ) exit;;
-  * ) echo "Please answer yes or no.";;
 esac
