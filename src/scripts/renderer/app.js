8c3885f231d7fb02e6752fa71fec4730ab90fb8c
Alexandru Rosianu <rosian_alex@yahoo.com>
2015-10-17 11:45:13 +0300

    Load the web app with a custom useragent string

--- src/scripts/renderer/app.js
+++ src/scripts/renderer/app.js
@@ -1,5 +1,17 @@
-// Remove the app and 'Electron' from the user agent
-// return userAgent
-//   .replace(new RegExp(this.manifest.productName + '/[\\S]*', 'g'), '')
-//   .replace(new RegExp('Electron/[\\S]*', 'g'), '')
-//   .replace(/[ ]+/g, ' ');
+const remote = require('remote');
+const manifest = remote.getGlobal('manifest');
+const webView = document.getElementById('view');
+
+// Set a custom UA and load the web app
+webView.setAttribute('useragent', getCleanUserAgent());
+webView.setAttribute('src', 'https://web.whatsapp.com/');
+
+/**
+ * Remove the app name and 'Electron' from the user agent.
+ */
+function getCleanUserAgent() {
+  return navigator.userAgent
+    .replace(new RegExp(manifest.productName + '/[\\S]*', 'g'), '')
+    .replace(new RegExp('Electron/[\\S]*', 'g'), '')
+    .replace(/[ ]+/g, ' ');
+}
