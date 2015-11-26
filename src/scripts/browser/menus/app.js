e321f7071b294c988526bb59257ddd8da4b5fee3
Himanshu <him.star@yahoo.ca>
2015-11-25 19:42:13 -0800

    Work on linking browser and renderer

--- src/scripts/browser/menus/app.js
+++ src/scripts/browser/menus/app.js
@@ -31,10 +31,25 @@ class AppMenu extends BaseMenu {
       shell.openExternal(menuItem.url);
     });
     
-    this.on('application:update-theme', function(menuItem) {
+    this.on('application:update-theme', function(themeFile) {
+        var x = 'console.log("'+themeFile.theme+'")';
+        BrowserWindow.getFocusedWindow().webContents.executeJavaScript(x);
+        //BrowserWindow.getFocusedWindow().console.log("hello");//BrowserWindow.webContents.getURL();
       //var wv = document.getElementById('view');
       //wv.insertCSS("body { display:none; }");
       //file:"../../../themes/Fluttery.css";*/
+      fs.readFile('src/themes/' + themeFile.theme + '.css', 'utf-8', function(err, cssFile) {
+      if (err) {
+        var e = 'console.log("'+err+'")';
+        BrowserWindow.getFocusedWindow().webContents.executeJavaScript(e);
+      } else {
+        //  BrowserWindow.getFocusedWindow().webContents.executeJavaScript('var wView = document.getElementById("view");wView.executeJavaScript("console.log(document.getElementsByTagName(/"head/")[0])");');
+          BrowserWindow.getFocusedWindow().webContents.executeJavaScript('wView.getElementsByTagName("head")[0].appendChild(wView.createElement("style"))');
+  var applyTheme = 'wView.getElementsByTagName("style")[0].innerHTML= "'+ cssFile +'"';
+  BrowserWindow.getFocusedWindow().webContents.executeJavaScript(applyTheme);
+  console.log(applyTheme);
+      }
+  });
     });
 
 
@@ -58,7 +73,31 @@ class AppMenu extends BaseMenu {
       BrowserWindow.getFocusedWindow().toggleDevTools();
     });
   }
+/**
+ * Reads the theme provided from src/themes/ and applies it to the page.
+ */
+applyTheme(themeFile) {
+  fs.readFile('src/themes/' + themeFile + '.css', 'utf-8', function(err, cssFile) {
+      if (err) {
+        console.error(err);
+      } else {
+        pushTheme(cssFile);
+      }
+  });
+               /* webView.executeJavaScript(x);
+                x = 'document.getElementsByTagName("style")[0].innerHTML= ".chat {background-color: #FFFACB;}"';
+                webView.executeJavaScript(x);*/
+}
 
+/**
+ * Appropriately applies the provided theme file code to the application.
+ */
+pushTheme(theme) {
+  BrowserWindow.getFocusedWindow().webContents.executeJavaScript('var wView = document.getElementById("view");wView.getElementsByTagName("head")[0].appendChild(wView.createElement("style"))');
+  var applyTheme = 'wView.getElementsByTagName("style")[0].innerHTML= "'+ theme +'"';
+  BrowserWindow.getFocusedWindow().webContents.executeJavaScript(applyTheme);
+  console.log(applyTheme);  
+}
 }
 
 export default AppMenu;
