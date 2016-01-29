7e5b11047bc70fc63cd8a6e3b025619f8adb48b0
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-01-29 14:09:19 +0200

    Adds kb shortcuts ctrl+tab for convo navigation

--- src/scripts/renderer/keymap.js
+++ src/scripts/renderer/keymap.js
@@ -4,14 +4,14 @@ import webView from './webview';
 log('binding keyboard shortcuts');
 
 // Previous chat
-Mousetrap.bind(['command+up', 'ctrl+up'], function() {
+Mousetrap.bind(['mod+up', 'ctrl+shift+tab'], function() {
   log('previous conversation');
   webView.send('switch-conversation', -1);
   return false;
 });
 
 // Next chat
-Mousetrap.bind(['command+down', 'ctrl+down'], function() {
+Mousetrap.bind(['mod+down', 'ctrl+tab'], function() {
   log('next conversation');
   webView.send('switch-conversation', +1);
   return false;
