1180f67a670716f4fa571455c2125b22ecbb19aa
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-02-08 11:00:29 +0200

    Improves/fixes log saving to file

--- src/scripts/browser/utils/logger.js
+++ src/scripts/browser/utils/logger.js
@@ -1,18 +1,28 @@
 import manifest from '../../../package.json';
+import stripAnsi from 'strip-ansi';
+import mkdirp from 'mkdirp';
 import debug from 'debug';
 import path from 'path';
 import util from 'util';
 import app from 'app';
 import fs from 'fs';
 
-const fileLogPath = path.join(app.getPath('appData'), 'logs', Date.now() + '.log');
-const fileLogStream = fs.createWriteStream(fileLogPath, {flags: 'a'});
 const fileLogEnabled = process.env.DEBUG && !process.mas;
+let fileLogStream = null;
 
 if (fileLogEnabled) {
+  const fileLogsDir = path.join(app.getPath('userData'), 'logs');
+  mkdirp.sync(fileLogsDir);
+
+  const fileLogPath = path.join(fileLogsDir, Date.now() + '.log');
+  fileLogStream = fs.createWriteStream(null, {fd: fs.openSync(fileLogPath, 'a')});
+
   process.on('exit', (code) => {
     fileLogStream.end('process exited with code ' + code + '\n');
+    fileLogStream = null;
   });
+
+  console.log('DEBUG is truthy, saving logs to', '"' + fileLogPath + '"');
 }
 
 export function debugLogger(filename) {
@@ -21,8 +31,8 @@ export function debugLogger(filename) {
   const logger = debug(namespace);
   logger.log = function() {
     console.error.apply(console, arguments);
-    if (fileLogEnabled) {
-      fileLogStream.write(util.format.apply(util, arguments) + '\n');
+    if (fileLogStream) {
+      fileLogStream.write(stripAnsi(util.format.apply(util, arguments)) + '\n');
     }
   };
   return logger;
@@ -32,11 +42,12 @@ export function errorLogger(filename, fatal) {
   const fakePagePath = filename.replace(app.getAppPath(), '');
   return function(...args) {
     args = args.map(a => a instanceof Error ? a.stack : a);
-    const argsMessage = util.format.apply(util, args);
+    const argsMessage = stripAnsi(util.format.apply(util, args));
 
-    console.error(`[${new Date().toUTCString()}] ${fakePagePath}:`, argsMessage);
-    if (fileLogEnabled) {
-      fileLogStream.write(argsMessage + '\n');
+    const errorPrefix = `[${new Date().toUTCString()}] ${fakePagePath}:`;
+    console.error(errorPrefix, argsMessage);
+    if (fileLogStream) {
+      fileLogStream.write(errorPrefix + ' ' + argsMessage + '\n');
     }
 
     const analytics = require('./analytics').default;
