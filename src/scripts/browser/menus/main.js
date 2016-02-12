85c443d16daec8d1999a6c25a2d1e4528077336a
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-02-12 22:46:02 +0200

    Fixes menus loader error

--- src/scripts/browser/menus/main.js
+++ src/scripts/browser/menus/main.js
@@ -3,7 +3,6 @@ import {parseTemplate} from './utils';
 const template = [
   './templates/main-app-darwin',
   './templates/main-app-nondarwin',
-  './templates/main-settings',
   './templates/main-edit',
   './templates/main-view',
   './templates/main-theme',
