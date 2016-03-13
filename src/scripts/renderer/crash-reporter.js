d62d94de90c1feeaf46d89f9a6d438ac40611246
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-03-13 14:57:00 +0200

    Makes CrashReporter failsafe in renderer too

--- src/scripts/renderer/crash-reporter.js
+++ src/scripts/renderer/crash-reporter.js
@@ -1,16 +1,20 @@
-import CrashReporter from 'crash-reporter';
 import remote from 'remote';
 
 const manifest = remote.getGlobal('manifest');
 
 if (!process.mas) {
-  log('starting crash reporter');
-  CrashReporter.start({
-    productName: manifest.productName,
-    companyName: manifest.win.companyName,
-    submitURL: manifest.crashReporter.url,
-    autoSubmit: true
-  });
+  if (manifest.crashReporter && manifest.crashReporter.url) {
+    log('starting crash reporter');
+    const CrashReporter = require('crash-reporter');
+    CrashReporter.start({
+      productName: manifest.productName,
+      companyName: manifest.win.companyName,
+      submitURL: manifest.crashReporter.url,
+      autoSubmit: true
+    });
+  } else {
+    log('crash reporter url not configured');
+  }
 } else {
   log('mas release');
 }
