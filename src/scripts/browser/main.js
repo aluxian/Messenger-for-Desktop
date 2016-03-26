e093872a9d8e9cca52af3af8a4de2d6fd61ba969
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-03-26 08:14:27 +0200

    Disable the crash reporter if reports are disabled

--- src/scripts/browser/main.js
+++ src/scripts/browser/main.js
@@ -145,26 +145,30 @@ process.on('uncaughtException', function(ex) {
   // Enable the crash reporter
   if (!process.mas) {
     if (manifest.crashReporter && manifest.crashReporter.url) {
-      app.on('will-finish-launching', function() {
-        log('will finish launching');
+      if (prefs.get('analytics-track')) {
+        app.on('will-finish-launching', function() {
+          log('will finish launching');
 
-        // Crash reporter
-        const reporterOptions = {
-          productName: manifest.productName,
-          companyName: manifest.win.companyName,
-          submitURL: manifest.crashReporter.url,
-          autoSubmit: true
-        };
+          // Crash reporter
+          const reporterOptions = {
+            productName: manifest.productName,
+            companyName: manifest.win.companyName,
+            submitURL: manifest.crashReporter.url,
+            autoSubmit: true
+          };
 
-        log('starting crash reporter', JSON.stringify(reporterOptions));
-        const CrashReporter = require('crash-reporter');
-        CrashReporter.start(reporterOptions);
-      });
+          log('starting crash reporter', JSON.stringify(reporterOptions));
+          const CrashReporter = require('crash-reporter');
+          CrashReporter.start(reporterOptions);
+        });
+      } else {
+        log('analytics disabled, so crash reporter disabled');
+      }
     } else {
       log('crash reporter url not configured');
     }
   } else {
-    log('mas release');
+    log('mas release, crash reporter disabled');
   }
 
   // Create the main app object and init
--- src/scripts/renderer/crash-reporter.js
+++ src/scripts/renderer/crash-reporter.js
@@ -1,17 +1,24 @@
 import remote from 'remote';
 
 const manifest = remote.getGlobal('manifest');
+const prefs = remote.require('../browser/utils/prefs').default;
 
 if (!process.mas) {
   if (manifest.crashReporter && manifest.crashReporter.url) {
-    log('starting crash reporter');
-    const CrashReporter = require('crash-reporter');
-    CrashReporter.start({
-      productName: manifest.productName,
-      companyName: manifest.win.companyName,
-      submitURL: manifest.crashReporter.url,
-      autoSubmit: true
-    });
+    if (prefs.get('analytics-track')) {
+      const reporterOptions = {
+        productName: manifest.productName,
+        companyName: manifest.win.companyName,
+        submitURL: manifest.crashReporter.url,
+        autoSubmit: true
+      };
+
+      log('starting crash reporter', JSON.stringify(reporterOptions));
+      const CrashReporter = require('crash-reporter');
+      CrashReporter.start(reporterOptions);
+    } else {
+      log('analytics disabled, so crash reporter disabled');
+    }
   } else {
     log('crash reporter url not configured');
   }
