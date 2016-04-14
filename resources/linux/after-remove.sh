6182f5c8767f8e22679eb1e99d2170b06375769a
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-04-14 17:11:47 +0300

    Move autostart .desktop to HOME after install

--- resources/linux/after-remove.sh
+++ resources/linux/after-remove.sh
@@ -3,5 +3,8 @@
 # Delete the link to the binary
 rm -f /usr/bin/{{ name }}
 
+# Delete app.desktop from $XDG_CONFIG_HOME
+rm -f "${XDG_CONFIG_HOME:-$HOME/.config}/autostart/{{ name }}.desktop"
+
 # Delete leftover app data
 rm -rf "${XDG_CONFIG_HOME:-$HOME/.config}/{{ productName }}"
