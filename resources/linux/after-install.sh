0757867a146d34460e9e80444302b123b271a3a8
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-04-16 13:34:20 +0300

    Let the app move whatsie.desktop on linux

--- resources/linux/after-install.sh
+++ resources/linux/after-install.sh
@@ -2,6 +2,3 @@
 
 # Link to the binary
 ln -sf /opt/{{ name }}/{{ name }} /usr/bin/{{ name }}
-
-# Copy app.desktop from /etc/xdg/autostart/ to $XDG_CONFIG_HOME
-cp /etc/xdg/autostart/{{ name }}.desktop "${XDG_CONFIG_HOME:-$HOME/.config}/autostart/{{ name }}.desktop"
--- src/scripts/browser/components/auto-launcher/impl-linux.js
+++ src/scripts/browser/components/auto-launcher/impl-linux.js
@@ -2,27 +2,46 @@ import manifest from '../../../../package.json';
 import async from 'async';
 import path from 'path';
 import app from 'app';
-import fs from 'fs';
+import fs from 'fs-extra';
 
 import BaseAutoLauncher from './base';
 
 const autoStartKey = 'X-GNOME-Autostart-enabled';
 const autoStartDir = path.join(app.getPath('home'), '.config', 'autostart');
 const desktopPath = path.join(autoStartDir, manifest.name + '.desktop');
+const systemDesktopPath = path.join('/etc/xdg/autostart/', );
 
 class LinuxAutoLauncher extends BaseAutoLauncher {
 
-  setKey(value, callback) {
-    log('setting', autoStartKey, value);
-    async.waterfall([
-      async.apply(fs.readFile, desktopPath, 'utf-8'),
-      (fileContent, callback) => {
-        const pattern = new RegExp(autoStartKey + '=.*');
-        const replaceWith = autoStartKey + '=' + value;
-        const newFileContent = fileContent.replace(pattern, replaceWith);
-        fs.writeFile(desktopPath, newFileContent, 'utf-8', callback);
+  ensureFileExists(callback) {
+    fs.access(desktopPath, fs.R_OK | fs.W_OK, (err) => {
+      if (err) { // no access / does not exist
+        fs.unlink(desktopPath, () => { // err ignored
+          fs.copy(systemDesktopPath, desktopPath, callback);
+        });
+      } else { // ok
+        callback(null);
       }
-    ], callback);
+    });
+  }
+
+  setKey(value, callback) {
+    this.ensureFileExists((ensureErr) => {
+      if (ensureErr) {
+        logError(ensureErr);
+      }
+
+      log('setting', autoStartKey, value);
+      async.waterfall([
+        async.apply(fs.readFile, desktopPath, 'utf-8'),
+        (fileContent, callback) => {
+          const pattern = new RegExp(autoStartKey + '=.*');
+          const replaceWith = autoStartKey + '=' + value;
+          const newFileContent = fileContent.replace(pattern, replaceWith);
+          fs.writeFile(desktopPath, newFileContent, 'utf-8', callback);
+        }
+      ], callback);
+    });
   }
 
   enable(callback) {
@@ -34,18 +53,24 @@ class LinuxAutoLauncher extends BaseAutoLauncher {
   }
 
   isEnabled(callback) {
-    log('checking key', autoStartKey);
-    fs.readFile(desktopPath, 'utf-8', function(err, file) {
-      if (err) {
-        return callback(err);
+    this.ensureFileExists((ensureErr) => {
+      if (ensureErr) {
+        logError(ensureErr);
       }
 
-      const pattern = new RegExp(autoStartKey + '=(.*)');
-      const matches = pattern.exec(file);
-      const enabled = matches && matches[1] == 'true';
+      log('checking key', autoStartKey);
+      fs.readFile(desktopPath, 'utf-8', function(err, file) {
+        if (err) {
+          return callback(err);
+        }
 
-      log(autoStartKey, 'is', enabled);
-      callback(null, enabled);
+        const pattern = new RegExp(autoStartKey + '=(.*)');
+        const matches = pattern.exec(file);
+        const enabled = matches && matches[1] == 'true';
+
+        log(autoStartKey, 'is', enabled);
+        callback(null, enabled);
+      });
     });
   }
 
