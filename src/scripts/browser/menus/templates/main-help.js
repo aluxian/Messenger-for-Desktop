891f226bd0e251eae8d70e8244fa6dbdac4909a0
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-04-20 14:39:08 +0300

    Refactor menu (Code Climate)

--- src/scripts/browser/menus/templates/main-help.js
+++ src/scripts/browser/menus/templates/main-help.js
@@ -1,66 +1,21 @@
 import $ from '../expressions';
+import g from '../generator';
 
 export default {
   label: '&Help',
   role: 'help',
-  submenu: [{
-    label: 'Ra&ffle Code',
-    click: $.openRaffleDialog()
-  }, {
-    label: 'Gitter &Chat',
-    click: $.all(
-      $.openUrl('https://gitter.im/Aluxian/Whatsie'),
-      $.analytics.trackEvent('Menu', 'Link', 'gitter_chat')
-    )
-  }, {
-    type: 'separator'
-  }, {
-    label: '&Write a Review',
-    click: $.all(
-      $.openUrl('https://aluxian.typeform.com/to/s0wi5P'),
-      $.analytics.trackEvent('Menu', 'Link', 'write_review')
-    )
-  }, {
-    label: '&Suggest a Feature',
-    click: $.all(
-      $.openUrl('https://aluxian.typeform.com/to/adWvdX'),
-      $.analytics.trackEvent('Menu', 'Link', 'suggest_feature')
-    )
-  }, {
-    label: '&Report an Issue',
-    click: $.all(
-      $.openUrl('https://aluxian.typeform.com/to/A30zq7'),
-      $.analytics.trackEvent('Menu', 'Link', 'report_issue')
-    )
-  }, {
-    type: 'separator'
-  }, {
-    label: '&Email Developer',
-    click: $.all(
-      $.openUrl('mailto:me@aluxian.com'),
-      $.analytics.trackEvent('Menu', 'Link', 'email_developer')
-    )
-  }, {
-    label: '&Tweet Developer',
-    click: $.all(
-      $.openUrl('https://twitter.com/Aluxian'),
-      $.analytics.trackEvent('Menu', 'Link', 'tweet_developer')
-    )
-  }, {
-    type: 'separator'
-  }, {
-    label: 'Donate &PayPal',
-    click: $.all(
-      $.openUrl('https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4YVCUBK2QJKBL'),
-      $.analytics.trackEvent('Menu', 'Link', 'donate_paypal'),
-      $.analytics.trackGoal(1, 1)
-    )
-  }, {
-    label: 'Donate &Bitcoin',
-    click: $.all(
-      $.openUrl('https://www.coinbase.com/Aluxian'),
-      $.analytics.trackEvent('Menu', 'Link', 'donate_bitcoin'),
-      $.analytics.trackGoal(1, 1)
-    )
-  }]
+  submenu: [
+    { label: 'Ra&ffle Code', click: $.openRaffleDialog() },
+    g.helpLink('Gitter &Chat', 'https://gitter.im/Aluxian/Whatsie', 'gitter_chat'),
+    g.separator(),
+    g.helpLink('&Write a Review', 'https://aluxian.typeform.com/to/s0wi5P', 'write_review'),
+    g.helpLink('&Suggest a Feature', 'https://aluxian.typeform.com/to/adWvdX', 'suggest_feature'),
+    g.helpLink('&Report an Issue', 'https://aluxian.typeform.com/to/A30zq7', 'report_issue'),
+    g.separator(),
+    g.helpLink('&Email Developer', 'mailto:me@aluxian.com', 'contact_developer_email'),
+    g.helpLink('&Tweet Developer', 'https://twitter.com/Aluxian', 'contact_developer_tweet'),
+    g.separator(),
+    g.helpDonate('&PayPal', 'https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=4YVCUBK2QJKBL', 'paypal'),
+    g.helpDonate('&Bitcoin', 'https://www.coinbase.com/Aluxian', 'bitcoin')
+  ]
 };
