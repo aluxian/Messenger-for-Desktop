4eaec16357006c3b68d5277764165afda7a87731
Emile Fugulin <emilefugulin@hotmail.com>
2016-06-04 10:58:57 -0400

    Remove config files and cache when uninstalling on linux

--- assets-linux/after-remove.sh
+++ assets-linux/after-remove.sh
@@ -7,4 +7,7 @@ rm -f /usr/local/bin/messengerfordesktop
 rm -f /usr/share/applications/messengerfordesktop.desktop
 
 # Config files
-rm -f -r ~/.config/Messenger
+rm -f -r /home/$SUDO_USER/.config/Messenger/
+
+# Cache
+rm -f -r /home/$SUDO_USER/.cache/Messenger
