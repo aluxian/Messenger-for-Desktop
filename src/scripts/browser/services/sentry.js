86dee8ecd6e865a7f4eb89bac4a3a82a32590795
Alexandru Rosianu <rosian_alex@yahoo.com>
2016-06-08 21:44:10 +0300

    Fix services/sentry export

--- src/scripts/browser/services/sentry.js
+++ src/scripts/browser/services/sentry.js
@@ -1,6 +1,6 @@
 import raven from 'raven';
 
-export default function getClient() {
+export function getClient() {
   return new raven.Client(global.manifest.sentry.dsn, {
     release: global.manifest.version,
     name: global.manifest.productName
