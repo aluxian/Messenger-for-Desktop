82d71de3e5e2733067e7ba02854c37b016d8d4e8
Alexandru Rosianu <me@aluxian.com>
2016-06-10 09:33:54 +0300

    Fix rebuild on Travis

--- scripts/darwin/rebuild_src.sh
+++ scripts/darwin/rebuild_src.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-export npm_config_target=`node -pe "require('./src/package.json').electronVersion.substring(1)"`
-cd src && HOME=~/.electron-gyp npm_config_disturl="https://atom.io/download/electron" npm_config_arch="x64" npm_config_runtime="electron" npm install && cd ..
+export npm_config_target=`node -pe "require('../../src/package.json').electronVersion.substring(1)"`
+cd src && HOME=~/.electron-gyp npm_config_disturl="https://atom.io/download/atom-shell" npm_config_arch="x64" npm_config_runtime="electron" npm install && cd ..
