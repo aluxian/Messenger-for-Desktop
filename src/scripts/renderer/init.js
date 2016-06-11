5b88e027b7c662c75d196e67fa9e3c4941a3ac01
Alexandru Rosianu <me@aluxian.com>
2016-06-11 19:14:12 +0300

    Expose log et al. in renderer and webview

--- src/scripts/renderer/init.js
+++ src/scripts/renderer/init.js
@@ -10,6 +10,12 @@ const {addPath} = require(path.join(appPath, 'node_modules', 'app-module-path'))
 addPath(path.join(appPath, 'scripts'));
 addPath(path.join(appPath, 'node_modules'));
 
+// Add loggers to be used in the console
+const logger = require('common/utils/logger');
+window.log = logger.debugLogger('console:renderer');
+window.logError = logger.errorLogger('console:renderer', false);
+window.logFatal = logger.errorLogger('console:renderer', true);
+
 require('renderer/main');
 require('renderer/webview');
 require('renderer/components/keymap');
--- src/scripts/renderer/preload/init.js
+++ src/scripts/renderer/preload/init.js
@@ -10,4 +10,10 @@ const {addPath} = require(path.join(appPath, 'node_modules', 'app-module-path'))
 addPath(path.join(appPath, 'scripts'));
 addPath(path.join(appPath, 'node_modules'));
 
+// Add loggers to be used in the console
+const logger = require('common/utils/logger');
+window.log = logger.debugLogger('console:webview');
+window.logError = logger.errorLogger('console:webview', false);
+window.logFatal = logger.errorLogger('console:webview', true);
+
 require('renderer/preload');
