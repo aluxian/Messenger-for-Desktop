57990691af00f2b6e1adc056081104a9f13701db
Alexandru Rosianu <me@aluxian.com>
2016-06-13 14:59:35 +0300

    Remove bash -v from travis/install_nvm.sh

--- scripts/darwin/install_nvm.sh
+++ scripts/darwin/install_nvm.sh
@@ -1,11 +1,6 @@
-#!/bin/bash -ev
+#!/bin/bash -e
 
 git clone https://github.com/creationix/nvm.git /tmp/.nvm
-
-echo source /tmp/.nvm/nvm.sh
-set +v
 source /tmp/.nvm/nvm.sh
-set -v
-
 nvm install 0.12
 nvm use 0.12
