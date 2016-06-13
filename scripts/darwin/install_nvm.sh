5e62cc00b30dfd1f2441ec613d01a8a99359ede5
Alexandru Rosianu <me@aluxian.com>
2016-06-13 14:44:02 +0300

    Improve travis build scripts

--- scripts/darwin/install_nvm.sh
+++ scripts/darwin/install_nvm.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ev
 
 git clone https://github.com/creationix/nvm.git /tmp/.nvm
+
+echo source /tmp/.nvm/nvm.sh
+set +v
 source /tmp/.nvm/nvm.sh
+set -v
+
 nvm install 0.12
 nvm use 0.12
