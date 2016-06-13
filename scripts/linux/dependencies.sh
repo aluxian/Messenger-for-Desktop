57c08541048b358bfe8e5b180eb67c935c5a5431
Alexandru Rosianu <me@aluxian.com>
2016-06-13 13:33:58 +0300

    Try to fix CircleCI

--- scripts/linux/dependencies.sh
+++ scripts/linux/dependencies.sh
@@ -9,3 +9,8 @@ sudo apt-get install libc6-dev-i386 gcc-4.9-multilib g++-4.9-multilib \
   lib32stdc++-4.9-dev lib32gcc-4.9-dev lib32asan1 ruby-dev make rpm
 sudo ln -s /usr/include/asm-generic /usr/include/asm
 rvmsudo gem install fpm
+
+npm install
+cd src && npm install && cd ..
+gulp download:linux32 --verbose
+gulp download:linux64 --verbose
