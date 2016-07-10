aefa4b9027da217966e270e83de503f14836d95d
Alexandru Rosianu <me@aluxian.com>
2016-07-10 10:35:42 +0300

    Fix context menu

--- src/scripts/renderer/preload/listeners.js
+++ src/scripts/renderer/preload/listeners.js
@@ -4,7 +4,7 @@ import platform from 'common/utils/platform';
 
 // Forward context menu opens
 remote.getCurrentWebContents().on('context-menu', function (event, params) {
-  params.isWindows7 = platform.isWindows7();
+  params.isWindows7 = platform.isWindows7;
   params = JSON.stringify(params);
   log('sending context menu', params);
   ipcRenderer.send('context-menu', params);
