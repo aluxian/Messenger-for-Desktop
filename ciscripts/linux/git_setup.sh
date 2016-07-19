3d9d2681022128f6c7d42dfed2af62164983b4e3
Alexandru Rosianu <me@aluxian.com>
2016-07-19 20:18:36 +0300

    Squelch git message

--- ciscripts/linux/git_setup.sh
+++ ciscripts/linux/git_setup.sh
@@ -2,6 +2,7 @@
 
 git config --global user.email $GIT_USER_EMAIL
 git config --global user.name $GIT_USER_NAME
+git config --global push.default simple
 
 aws s3 cp --region eu-west-1 s3://aluxian/sshkeys/aur ~/.ssh/aur
 aws s3 cp --region eu-west-1 s3://aluxian/sshkeys/aur.pub ~/.ssh/aur.pub
