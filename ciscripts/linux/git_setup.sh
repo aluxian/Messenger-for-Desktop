959940427e760c52bf93a4cf2945074f615cb0b1
Alexandru Rosianu <me@aluxian.com>
2016-07-19 11:10:13 +0300

    Fix private key permissions (Circle CI)

--- ciscripts/linux/git_setup.sh
+++ ciscripts/linux/git_setup.sh
@@ -3,7 +3,9 @@
 aws s3 cp --region eu-west-1 s3://aluxian/sshkeys/aur ~/.ssh/aur
 aws s3 cp --region eu-west-1 s3://aluxian/sshkeys/aur.pub ~/.ssh/aur.pub
 
+chmod 600 ~/.ssh/aur
 touch ~/.ssh/config
+
 echo "Host aur.archlinux.org" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/aur" >> ~/.ssh/config
 echo "  User aur" >> ~/.ssh/config
