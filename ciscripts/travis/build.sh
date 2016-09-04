beb85d28c10f9119b6334e35169f1bc7b454cf00
Alexandru Rosianu <me@aluxian.com>
2016-09-04 20:56:12 +0300

    Fix Travis builds

--- ciscripts/travis/build.sh
+++ ciscripts/travis/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -ev
 
+./ciscripts/travis/install_aws.sh
 ./ciscripts/travis/import_cert.sh
-gulp rebuild:64 --prod --verbose
+
+gulp rebuild:64 --verbose
 gulp pack:darwin64:dmg --prod --verbose
 gulp pack:darwin64:zip --prod --verbose
