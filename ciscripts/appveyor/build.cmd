1f4a453e353281a563f1dc3831acf8c9dae96c83
Alexandru Rosianu <me@aluxian.com>
2016-09-06 23:18:48 +0300

    Build nsis, too, on AppVeyor

--- ciscripts/appveyor/build.cmd
+++ ciscripts/appveyor/build.cmd
@@ -8,3 +8,5 @@ CALL gulp rebuild:32 --verbose
 CALL gulp pack:win32:portable --prod --verbose
 CALL gulp pack:win32:installer --prod --verbose
 CALL gulp clean:prev-releases:win32 --verbose
+CALL gulp pack:win32:nsis --prod --verbose
+CALL gulp clean:prev-releases:win32 --verbose
