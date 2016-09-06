a5af82d3cd9ee59ad90aee7344d2a818cac50aab
Alexandru Rosianu <me@aluxian.com>
2016-09-06 12:50:30 +0300

    Add 'npm install' to Travis

--- ciscripts/travis/build.sh
+++ ciscripts/travis/build.sh
@@ -3,6 +3,8 @@
 ./ciscripts/travis/install_aws.sh
 ./ciscripts/travis/import_cert.sh
 
+cd src && npm install && cd ..
+
 gulp rebuild:64 --verbose
 gulp pack:darwin64:dmg --prod --verbose
 gulp pack:darwin64:zip --prod --verbose
