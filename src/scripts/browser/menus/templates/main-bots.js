2a7913b5c4de35937aa129138fb9177ee626f171
Alexandru Rosianu <me@aluxian.com>
2017-06-11 17:12:19 +0100

    Add needsWindow:true to new bot menu

--- src/scripts/browser/menus/templates/main-bots.js
+++ src/scripts/browser/menus/templates/main-bots.js
@@ -4,6 +4,7 @@ export default {
   label: '&Bots',
   submenu: [{
     label: 'Tomo \t\t Cheap flight alerts, save 70%',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('tomocheapflights?ref=MFD_APP')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'tomocheapflights')
@@ -12,18 +13,21 @@ export default {
     type: 'separator'
   }, {
     label: 'Poncho \t\t Personal weather forecasts',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('hiponcho')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'hiponcho')
     )
   }, {
     label: 'TechCrunch \t Personalized tech news',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('techcrunch')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'techcrunch')
     )
   }, {
     label: 'CNN \t\t Breaking news alerts',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('cnn')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'cnn')
@@ -32,24 +36,28 @@ export default {
     type: 'separator'
   }, {
     label: 'Foxsy \t\t Make new friends',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('foxsybot')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'foxsybot')
     )
   }, {
     label: 'Icon8 \t\t Turn your selfies into art',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('icon8bot')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'icon8bot')
     )
   }, {
     label: 'Sensay \t\t On-demand help from humans',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('sensaybot')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'sensaybot')
     )
   }, {
     label: 'Swelly \t\t Better decisions with friends',
+    needsWindow: true,
     click: $.all(
       $.sendToWebView('open-messenger-thread', $.val('swell.bot')),
       $.analytics.trackEvent('Menu', 'Open Bot', 'swell.bot')
